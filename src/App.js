7e6f99eebfd15aa25358ed254cc22cab865dc58c
bgriner <bgriner@shutterfly.com>
2021-02-16 17:30:06 -0700

    Created sign in page with sign in component and sign in with google

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import {Switch, Route} from 'react-router-dom'
+import {Switch, Route} from 'react-router-dom';
 import './App.css';
 import HomePage from "./pages/homepage/homepage.component";
-import ShopPage from "./pages/shop/shop.components"
-import Header from "./components/header/header.component"
-import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component"
+import ShopPage from "./pages/shop/shop.components";
+import Header from "./components/header/header.component";
+import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
 
 import {auth} from './firebase/firebase.utils'
 
@@ -62,7 +62,7 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <Header/>
+        <Header currentUser={this.state.currentUser} />
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route path='/shop/hats' component={HatsPage} />
